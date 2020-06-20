cf676a48297a0e34dcae88d84043e45f7289c46c
zz-ming <58175246+zz-ming@users.noreply.github.com>
2020-06-21 01:06:47 +0800

    Create Buff.h

---
+++ Buff.h
@@ -0,0 +1,35 @@
+#ifndef __BUFF_H__
+#define __BUFF_H__
+
+#include<iostream>
+
+enum BuffType
+{
+	Poisoning,//中毒
+	Freezing,//冰冻
+	Burning,//烧伤
+	SpeedUp,//加速
+	SpeedDown,//减速
+};
+
+class Buff
+{
+public:
+	BuffType buffType;//buff类型
+	float durations;//持续时间
+
+	Buff(BuffType buff, int time);
+	//~Buff();
+	bool operator ==(const Buff &b);
+};
+
+extern Buff poisoning;
+extern Buff freezing;
+extern Buff burning;
+extern Buff speedUp;
+extern Buff speedDown;
+
+
+
+#endif // !__BUFF_H__
+

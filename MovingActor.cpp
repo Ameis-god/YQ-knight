328dcd676a74884171a1b397abc740c98f3f3652
zz-ming <58175246+zz-ming@users.noreply.github.com>
2020-06-21 23:38:55 +0800

    Create MovingActor.cpp

---
+++ MovingActor.cpp
@@ -0,0 +1,46 @@
+#include "MovingActor.h"
+
+bool MovingActor::takeBuff(const Buff b)
+{
+	if (myBuff.empty())
+	{
+		myBuff.push_back(b);
+		scheduleOnce(schedule_selector(MovingActor::clearBuff), b.durations);
+		return true;
+	}
+
+	for (auto it = myBuff.begin(); it != myBuff.end(); it++)
+	{
+		if ((*it) == b)
+		{
+			return false;
+		}
+		//无此buff时进行添加
+		else
+		{
+			myBuff.push_back(b);
+			scheduleOnce(schedule_selector(MovingActor::clearBuff), b.durations);
+			return true;
+		}
+	}
+
+}
+
+void MovingActor::clearBuff(float dt)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (auto it = myBuff.begin(); it != myBuff.end(); it++)
+		{
+			if ((*it).durations <= 0)
+			{
+				myBuff.erase(it);
+				break;
+			}
+		}
+	}
+}
+ 
+float MovingActor::getMovingSpeed() {
+	return movingSpeed;
+}

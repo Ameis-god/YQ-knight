9ea2499588672d8fde5749e9d7c904974f0836de
zz-ming <58175246+zz-ming@users.noreply.github.com>
2020-06-21 23:38:19 +0800

    Create MovingActor.h

---
+++ MovingActor.h
@@ -0,0 +1,35 @@
+#ifndef __MOVINGACTOR_H__
+#define __MOVINGACTOR_H__
+
+#include<iostream>
+#include<vector>
+#include "cocos2d.h"
+#include "Buff.h"
+using namespace cocos2d;
+
+class MovingActor :public Sprite
+{
+protected:
+	int HP;//当前血量
+	int limitHP;//血量上限
+	int AD;//攻击力
+	int movingSpeed;//移动速度
+
+	Scene* currentScene;//记录属性条的放置
+
+	std::vector<Buff> myBuff;//buff栏
+public:
+	virtual bool init() = 0;//创建
+	virtual void die() = 0;//死亡
+	virtual void attacked(const int value) = 0;//受到攻击
+	virtual void updateStatus(float dt) = 0;//更新状态
+
+	bool takeBuff(const Buff b);//获得buff
+	void clearBuff(float dt);
+	float getMovingSpeed() ;
+};
+
+
+#endif // !__MOVINGACTOR_H__
+
+
